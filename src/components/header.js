68d93592ac66f84bc53ba5986429cb4e67d454d5
Israel Z. Kollie <israel@celcom.local>
2019-05-07 21:20:30 +0000

    Initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,26 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
+const Header = () => {
+    return (
+        <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+          <div class="container"> 
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span className="sr-only">Toggle navigation</span> <span className="icon-bar"></span> <span className="icon-bar"></span> <span className="icon-bar"></span> </button>
+              <a style={{ color: '#362929' }} className="navbar-brand page-scroll" href="#page-top">RCS <span style={{ fontSize: '20px' }}>Inc.</span></a> 
+              <div style={{ fontWeight: 'bold', fontSize: '20px' }} className="phone"><span style={{ color: '#362929' }}>Call Us Today</span>+231-775-800-179</div>
+            </div>
+            
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="#about" className="page-scroll">About</a></li>
+                <li><a href="#services" className="page-scroll">Services</a></li>
+                <li><a href="#portfolio" className="page-scroll">Projects</a></li>
+                <li><a href="#contact" className="page-scroll">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+      </nav>
+    )
+} 
 
 export default Header
