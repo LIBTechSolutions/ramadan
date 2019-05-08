c4446e153ef2704d038f3b5374861033a4ba911e
Israel Z. Kollie <israel@celcom.local>
2019-05-08 00:09:07 +0000

    add google fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,9 +12,11 @@ import { StaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 // import "../css/bootstrap.css"
-// import "../css/nivo-lightbox/nivo-lightbox.css"
-// import "../css/nivo-lightbox/default.css"
-// import "../fonts/font-awesome/css/font-awesome.css"
+import "../css/nivo-lightbox/nivo-lightbox.css"
+import "../css/nivo-lightbox/default.css"
+import "../fonts/font-awesome/css/font-awesome.css"
+require('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700');
+require('https://fonts.googleapis.com/css?family=Montserrat:400,700');
 import Footer from "./Footer";
 
 const Layout = ({ children }) => (
